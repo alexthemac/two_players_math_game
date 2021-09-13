ef7fc649992d489edd544a83d92081d8a1aaeccc
Alex MacLure <almaclure@gmail.com>
2021-09-13 16:34:02 +0000

    Initial project setup

---
+++ main.rb
@@ -0,0 +1,22 @@
+players class
+lifes state
+initialize with 3 lifes
+function to remove lifes
+initialiez unique identifier per player
+(require in game)
+
+player1 instance
+player2 instance
+
+game class 
+function to check when game is over
+function to set player (if current player...set to other player)
+state of current player
+answer = correct function
+(require in main)
+
+question class
+random number a and b constructor
+generate answer as well constructor
+generate question function (puts with a & b)
+(require in game)
